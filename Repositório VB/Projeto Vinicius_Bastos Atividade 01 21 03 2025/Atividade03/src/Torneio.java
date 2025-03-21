9b5ba388b7b811e8dc9ba0a1aaa906f488faca16
ViniciusDevlop <168871768+ViniciusDevlop@users.noreply.github.com>
2025-03-21 15:59:49 -0300

    Update Torneio.java

--- Repositório VB/Projeto Vinicius_Bastos Atividade 01 21 03 2025/Atividade03/src/Torneio.java
+++ Repositório VB/Projeto Vinicius_Bastos Atividade 01 21 03 2025/Atividade03/src/Torneio.java
@@ -1,1 +1,56 @@
+public class Torneio {
+    private String nome;
+    private int idade;
 
+    public Torneio(String nome, int idade) {
+        this.nome = nome;
+        this.idade = idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String verificarCategoria() {
+        if (idade >= 5 && idade <= 7) {
+            return "Infantil";
+        } else if (idade >= 8 && idade <= 10) {
+            return "Juvenil";
+        } else if (idade >= 11 && idade <= 15) {
+            return "Adolescente";
+        } else if (idade >= 16 && idade <= 30) {
+            return "Adulto";
+        } else {
+            return "Senior";
+        }
+    }
+
+    public void ImprimirDados() {
+        System.out.println("Nome: " + nome);
+        System.out.println("Idade: " + idade);
+        System.out.println("Categoria: " + verificarCategoria());
+    }
+}
+
+
+
+
+
+
+
+
+
+  
+}
