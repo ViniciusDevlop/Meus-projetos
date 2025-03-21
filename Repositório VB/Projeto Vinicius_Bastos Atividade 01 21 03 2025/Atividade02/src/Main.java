4d0559e5b121b034b82f1925d126454f04325ccc
ViniciusDevlop <168871768+ViniciusDevlop@users.noreply.github.com>
2025-03-21 11:15:18 -0300

    Add files via upload

--- Repositório VB/Projeto Vinicius_Bastos Atividade 01 21 03 2025/Atividade02/src/Main.java
+++ Repositório VB/Projeto Vinicius_Bastos Atividade 01 21 03 2025/Atividade02/src/Main.java
@@ -1,15 +1,43 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
 public class Main {
+    Scanner ent = new Scanner (System.in);
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+    Triangulo Triangulo1 = new Triangulo(5.0,8.0);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        System.out.println ("Dados do Triangulo1: ");
+        Triangulo1.imprimeDados();
+
+
+        Triangulo Triangulo2 = new Triangulo();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
+
+
 }
--- Repositório VB/Projeto Vinicius_Bastos Atividade 01 21 03 2025/Atividade02/src/Triangulo.java
+++ Repositório VB/Projeto Vinicius_Bastos Atividade 01 21 03 2025/Atividade02/src/Triangulo.java
@@ -1,2 +1,37 @@
 public class Triangulo {
+  private double base;
+  private double altura;
+
+  // Construtor padrão (sem parâmetros)
+    public Triangulo() {
+    }
+
+    // Construtor padrão (com parâmetros)
+    public Triangulo(double base, double altura) {
+        this.base = base;
+        this.altura = altura;
+
+    }
+public double getBase(){
+  return base;
+}
+public void setBase(double base){
+   this.base = base;
+}
+public double getAltura(){
+    return altura;
+}
+public void setAltura(double altura){
+    this.altura= altura;
+}
+
+public double calcularArea(){
+   return base * altura/2;
+}
+    // Metodo para imprimir os dados
+    public void imprimeDados() {
+        System.out.println("Base: " + base);
+        System.out.println("Altura: " + altura);
+        System.out.println("Área: " + calcularArea());
+    }
 }
