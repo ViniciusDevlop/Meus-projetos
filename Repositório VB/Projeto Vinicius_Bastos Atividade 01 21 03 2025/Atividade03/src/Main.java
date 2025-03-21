f5777f67e661e1ea69e5d147061bbfcb27a74910
ViniciusDevlop <168871768+ViniciusDevlop@users.noreply.github.com>
2025-03-21 16:00:30 -0300

    Update Main.java

--- Repositório VB/Projeto Vinicius_Bastos Atividade 01 21 03 2025/Atividade03/src/Main.java
+++ Repositório VB/Projeto Vinicius_Bastos Atividade 01 21 03 2025/Atividade03/src/Main.java
@@ -1,15 +1,16 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Torneio atleta1 = new Torneio("Joao", 10);
+        Torneio atleta2 = new Torneio("Mario", 18);
+        Torneio atleta3 = new Torneio("Marcos", 30);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        System.out.println("Dados do atleta1: ");
+        atleta1.ImprimirDados();
+
+        System.out.println("Dados do atleta2: ");
+        atleta2.ImprimirDados();
+
+        System.out.println("Dados do atleta3: ");
+        atleta3.ImprimirDados();
     }
 }
