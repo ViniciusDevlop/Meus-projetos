7cc711295ebce62caf6d14cc1ecca03029bc931f
ViniciusDevlop <168871768+ViniciusDevlop@users.noreply.github.com>
2025-03-21 14:12:52 -0300

    Update Main.java

--- Repositório VB/Projeto Vinicius_Bastos Atividade 01 21 03 2025/Atividade02/src/Main.java
+++ Repositório VB/Projeto Vinicius_Bastos Atividade 01 21 03 2025/Atividade02/src/Main.java
@@ -1,7 +1,8 @@
 import java.util.Scanner;
 public class Main {
     Scanner ent = new Scanner (System.in);
-    public static void main(String[] args) {
+public static void main(String[] args) {
+    Main main = new Main();
     Triangulo Triangulo1 = new Triangulo(5.0,8.0);
 
         System.out.println ("Dados do Triangulo1: ");
@@ -10,13 +11,20 @@ public class Main {
 
         Triangulo Triangulo2 = new Triangulo();
         System.out.println("Digite a Base do Triangulo 2: ");
-        double base= ent.nextDouble();
+        double base= main.ent.nextDouble();
 
         System.out.println("Digite a altura do Triangulo 2: ");
-        double altura = ent.nextDouble();
-        
+        double altura = main.ent.nextDouble();
+ 
+        Triangulo2.setBase(base);
+        Triangulo2.setAltura(altura);
 
+        System.out.println("Dados do Triangulo2: ");
+        Triangulo2.imprimeDados();
 
+        main.ent.close(); 
+ }
+}
 
 
 
