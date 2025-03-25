6dd5fb69ac9be6aaeb46d78552bfd50e4b2ebdc8
ViniciusDevlop <168871768+ViniciusDevlop@users.noreply.github.com>
2025-03-25 17:27:12 -0300

    Update dados.sql

--- Projetos VB/dados.sql
+++ Projetos VB/dados.sql
@@ -1,13 +1,13 @@
 create database meuBanco;
-
+/*isso cria um banco de dados com nome meuBanco */
 use meuBanco;
-
+//criando uma tabela com dados
 create table dados (
 nome char(50),
 idade int(2),
 cargo varchar(50)
 );
-
+//inserindo dados na tabela
 insert into dados(nome,idade,cargo) values
 ('Carlos','30','Capitão'),
 ('Marta','35','Tenente'),
@@ -16,5 +16,5 @@ insert into dados(nome,idade,cargo) values
 ('Márcio','28','Cabo'),
 ('Marcela','38','Major'),
 ('Marcelo','50','Marechal');
-
+//seleciona tudo que está na tabela
 select * from dados;
