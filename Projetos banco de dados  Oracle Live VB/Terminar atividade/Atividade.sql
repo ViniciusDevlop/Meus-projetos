48dea484362a2bffe5339b3ef3c3b3b2266cc452
Vinicius Bastos <168871768+ViniciusDevlop@users.noreply.github.com>
2025-05-06 16:52:00 -0300

    Update and rename os códigos.sql to Atividade.sql

--- Projetos banco de dados  Oracle Live VB/Terminar atividade/Atividade.sql
+++ Projetos banco de dados  Oracle Live VB/Terminar atividade/Atividade.sql
@@ -1,3 +1,5 @@
+-- todos os exercícios estão no arquivo doc.
+-- Exercicio 1 criar todas as tabelas
 -- Comando SQL para criar a tabela DEPTO
 CREATE TABLE DEPTO (
     cod_depto NUMBER(4) PRIMARY KEY,
@@ -31,6 +33,7 @@ CREATE TABLE LOCAL (
     cod_loc NUMBER(4) PRIMARY KEY,
     nome VARCHAR2(25) NOT NULL
 );
+--Exercicio 2 - Selecionar todas as tabelas
 -- Seleciona todos os registros e colunas da tabela DEPTO
 SELECT * FROM DEPTO;
 
@@ -42,6 +45,7 @@ SELECT * FROM CARGO;
 
 -- Seleciona todos os registros e colunas da tabela LOCAL
 SELECT * FROM LOCAL;
+--Exercício 3 colocar os dados dentro da tabela local
 --Inserindo dados no Local
 INSERT INTO LOCAL (COD_LOC,NOME ) VALUES (1, 'São Paulo');
 INSERT INTO LOCAL (cod_loc, nome) VALUES (2, 'Belo Horizonte');
@@ -50,10 +54,25 @@ INSERT INTO LOCAL (cod_loc, nome) VALUES (4, 'Campinas');
 INSERT INTO LOCAL (cod_loc, nome) VALUES (5, 'Curitiba');
 INSERT INTO LOCAL (cod_loc, nome) VALUES (6, 'Rio de Janeiro');
 
---inserindo dados no dpto
+--Exercicio 4 inserir dados na tabela dpto
+-- inserindo dados no dpto
 INSERT INTO DEPTO (cod_depto, nome, cod_loc) VALUES (10, 'Contabilidade', 2);
 INSERT INTO DEPTO (cod_depto, nome, cod_loc) VALUES (20, 'Controladoria', 1);
 INSERT INTO DEPTO (cod_depto, nome, cod_loc) VALUES (99, 'Financeiro', 4);
 INSERT INTO DEPTO (cod_depto, nome, cod_loc) VALUES (22, 'TI', 3);
 INSERT INTO DEPTO (cod_depto, nome, cod_loc) VALUES (25, 'Marketing', 7);
+
+--Exercício 5 
+-- Qual o problema ocorrido com a inserção do departamento de código 25? Como seria possível resolver o problema?
+-- R: Porque não há um indice 7 na tabela local, e isso é o que causa o conflito no código. Uma solução para isso é ou trocar o indice do departamento 
+-- ou adicionar mais uma localidade com um novo cod_loc e novo nome, trocar o código de algumas das cidades vigentes para o número do departamento.
+
+--Exercício 6 Faça a inclusão do departamento 25.
+INSERT INTO DEPTO (cod_depto, nome, cod_loc) VALUES (10, 'Contabilidade', 2);
+INSERT INTO DEPTO (cod_depto, nome, cod_loc) VALUES (20, 'Controladoria', 1);
+INSERT INTO DEPTO (cod_depto, nome, cod_loc) VALUES (99, 'Financeiro', 4);
+INSERT INTO DEPTO (cod_depto, nome, cod_loc) VALUES (22, 'TI', 3);
+INSERT INTO DEPTO (cod_depto, nome, cod_loc) VALUES (25, 'Marketing', 5);
+
+-- Exercício 8 Faça os testes de PK e FK nas tabelas
 
